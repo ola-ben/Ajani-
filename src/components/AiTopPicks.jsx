1a0afac4a595e711f53a2f8799e053ac6c82b39a
ola-ben <olaben09@gmail.com>
2025-10-09 16:15:38 +0100

    push

--- src/components/AiTopPicks.jsx
+++ src/components/AiTopPicks.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDirectoryData } from "../Hooks/useDirectoryData";
+import { useDirectoryData } from "../hook/useDirectoryData";
 
 // ✅ Hardcoded fallback images
 const FALLBACK_IMAGES = {
